559fcddaeae0f690dc0b684f6b5105385bc3167f
elsayed136 <Elsayed_Ahmed_Eltaweel@outlook.com>
2022-02-21 22:21:52 +0200

    Added: NewExpense & ExpenseForm and filter comp.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Expenses.css";
 import Card from "../UI/Card";
 
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2021");
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
     <Card className='expenses'>
+      <ExpensesFilter onChangeFilter={filterChangeHandler} />
       {props.items.map(item => (
         <ExpenseItem key={item.id} {...item} />
       ))}
