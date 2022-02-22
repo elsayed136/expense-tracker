da70800a9d3ab2092882740afaa640f85037e0c0
elsayed136 <Elsayed_Ahmed_Eltaweel@outlook.com>
2022-02-22 23:37:53 +0200

    Add:functionality of adding new expense and filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,16 +6,20 @@ import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
-  const [filteredYear, setFilteredYear] = useState("2021");
+  const [filteredYear, setFilteredYear] = useState("2022");
 
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter(
+    expense => expense.date.getFullYear().toString() === filteredYear
+  );
+
   return (
     <Card className='expenses'>
       <ExpensesFilter onChangeFilter={filterChangeHandler} />
-      {props.items.map(item => (
+      {filteredExpenses.map(item => (
         <ExpenseItem key={item.id} {...item} />
       ))}
     </Card>
